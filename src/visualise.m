ab53adf08efb4cc89c102eeab66c35ac852a71bc
rudolfspetrovs <rudolfs.petrovs@lu.lv>
2019-05-12 15:00:47 +0100

    A correction to correctly give error for old Matlab versions

--- src/visualise.m
+++ src/visualise.m
@@ -14,9 +14,10 @@ addOptional(p,'url',defaultUrl,validText);
 addOptional(p,'show',defaultShow,@islogical);
 parse(p,modelFile,svgFile,outputFile,varargin{:});
 
-if verLessThan('matlab','9.4')
-    % -- Code to run in MATLAB R2017b and earlier here --
-    % error('Matlab 9.4 (R2018a) or higher is required.');
+if verLessThan('matlab','9.1')
+    error('Matlab 9.1 (R2016b) or higher is required.');
+elseif verLessThan('matlab','9.4')
+    % -- Code to run in MATLAB R2016b - R2017b --
     response = visualiseInR2016b(p.Results.modelFile, p.Results.svgFile, p.Results.outputFile, p.Results.analysisType, p.Results.url, p.Results.show);
 else
     % -- Code to run in MATLAB R2018a and later here --
