a0a6ba70d2f83c22f5a15b008ae7c6b6d359daa4
rudolfspetrovs <rudolfs.petrovs@lu.lv>
2019-06-16 22:12:57 +0100

    For MacOS and Windows added opening of result file in Matlab browser

--- src/saveAndShowResult.m
+++ src/saveAndShowResult.m
@@ -3,6 +3,17 @@ fid = fopen(outputFile,'w');
 fwrite(fid, response.Body.Data);
 fclose(fid);
 if show
+    if ismac
+        % Code to run on Mac platform
+        web(outputFile);
+    elseif isunix
+        % Code to run on Linux platform
+    elseif ispc
+        % Code to run on Windows platform
+        web(outputFile);
+    else
+        disp('Platform not supported')
+    end
     web(outputFile, '-browser');
 end
 end
